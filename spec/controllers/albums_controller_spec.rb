e509b3ec7ec44bf9b912d1dc6deca6fb5a5fd601
Reggie Davis <reginald.davis88@gmail.com>
2016-08-18 00:44:44 -0500

    Added Album Controller/Test, Added/Changed Routes, & added to README

--- spec/controllers/albums_controller_spec.rb
+++ spec/controllers/albums_controller_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe AlbumsController, type: :controller do
+
+  describe "GET index" do
+    let(:artist) { FactoryGirl.create(:artist) }
+    let(:album) { FactoryGirl.create(:album) }
+
+    it "returns a list of albums belonging to artist" do
+      get :index
+      album.should belong_to(artist)
+    end
+  end
+
+end
